80ec5df4654dd39bbe7f87a41b320bbfc6577c93
paulofreitas-py <paulofreitas.py@protonmail.com>
2021-07-23 21:58:04 -0300

    mod08

--- [HC] Módulo 08 - GraphQL/src/main.js
+++ [HC] Módulo 08 - GraphQL/src/main.js
@@ -1,18 +1,31 @@
-import { creatServer} from 'http';
+import { createServer } from 'http';
 import { deflate } from 'zlib';
 
-    const server = creatServer((request, response) =>{
-        switch(request.url){
-            case '/status': {
+const server = createServer((request, response) =>{
+    switch(request.url){
+        case '/status': {
                 response.writeHead(200,{
-                    'content-Type': 'application/json'
+                    'Content-Type': 'application/json'
                 });
                 response.write(
                     JSON.stringify({status:'Okay'})
                 );
                 response.end();
+                return;
             }
-        default;
-        }
-    default:
-    })
+        default:
+            {
+                response.writeHead(404,'Service not Found.');
+                // response.write('Okay');
+                response.end(); 
+            }
+    }
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.HOSTNAME || '127.0.0.1';
+
+server.listen(8000,'127.0.0.1',()=>
+{
+    console.log(`Server listening at http://${HOSTNAME}:${PORT}`);
+})
