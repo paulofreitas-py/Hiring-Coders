060ad1c10857c3b0b8506dd276650fba3c6c2ab7
paulofreitas-py <paulofreitas.py@protonmail.com>
2021-07-28 11:18:31 -0300

    aula-16

---
+++ [HC] Módulo 09 - Node.JS Básico/nodejs-typescript/index.ts
@@ -0,0 +1,37 @@
+import { parse } from 'query-string';
+import * as url from 'url'
+import { writeFile } from 'fs';
+import { createServer, ServerResponse, IncomingMessage } from 'http';
+
+const port = 5000;
+
+const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+
+    const urlparse = url.parse(request.url ? request.url : '', true);
+    const params = parse(urlparse.search ? urlparse.search : '');
+
+    let resposta;
+
+    if (urlparse.pathname == '/criar-atualizar-usuario') {
+        // Criar um usuario/atualizar usuario (resolvido junto pq o txt esta sendo sobrescrito)
+        // receber informacoes do usuario
+
+        // salvar as informacoes
+        writeFile('users/' + params.id + '.txt', JSON.stringify(params), function (err) {
+            if (err) throw err;
+            resposta = 'Usuario criado/atualizado com sucesso';
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'text/plain');
+            response.end(resposta);
+            console.log('saved!');
+        });
+
+    }
+
+
+});
+
+// Execucao
+server.listen(port, "127.0.0.1", () => {
+    console.log(`Server running on port ${port}`);
+});
